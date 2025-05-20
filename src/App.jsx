e9ad805006d6a0b95f8f321e6ffced6b81268772
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-05-20 16:13:31 -0500

    created loading screen, navbar, and mobilebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-
-import './App.css'
+import "./index.css";
+import "./App.css";
+import LoadingScreen from "./components/LoadingScreen";
+import { useState } from "react";
+import NavBar from "./components/NavBar";
+import MobileMenu from "./components/MobileMenu";
+import Home from "./components/sections/Home";
 
 function App() {
-  
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-     
+      {!hasLoaded && <LoadingScreen onComplete={() => setHasLoaded(true)} />}
+      <div
+        className={`min-h-screen transition-opacity duration-700 ${
+          hasLoaded ? "opacity-100" : "opacity-0"
+        } bg-black text-gray-100`}
+      >
+        <NavBar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Home/>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/sections/Home.jsx
@@ -0,0 +1,13 @@
+
+
+const Home = () => {
+  return (
+    <section id="home" className="min-h-screen flex items-center justify-center relative">
+      <div className="text-center z-10 px-4">
+        <h1 className="text-5xl md:text-7xl font-bold mb-6 bg-gradient-to-r from-blue-500 via-[#bb8476] to-pink-400 bg-clip-text leading-right text-transparent">Hi, I'm Sara Dev</h1>
+      </div>
+    </section>
+  )
+}
+
+export default Home
