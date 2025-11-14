5887c1676bf929330e95645f30219f085241432c
Sara Costa Cabral Mululo <saramululo@gmail.com>
2025-11-14 10:54:55 -0600

    fixed project descriptions

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -15,7 +15,7 @@ const Projects = () => {
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
             <h3 className="text-xl font-bold mb-2">PlanAhead App</h3>
             <p className="text-gray-400 mb-4">
-              Digital calendar where authenticated users can view and manage their tasks by month or by week.
+              Digital calendar app where authenticated users can view and manage their tasks by month or by week.
             </p>
             <div className="flex flex-wrap gap-2 mb-4">
               {["React", "Flask", "SQLite", "Bootstrap"].map(
@@ -116,6 +116,33 @@ const Projects = () => {
             </div>
             <div className="flex justify-between items-center">
               <a
+                href="https://github.com/saraccmululo/Website_Traffic_Analysis_Database"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[#b3472c] hover:text-[#915c4f] transition-colors my-4"
+              >
+                View Project →
+              </a>
+            </div>
+          </div>
+
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">  
+          <h3 className="text-xl font-bold mb-2">Chat Messaging Web App</h3>
+            <p className="text-gray-400 mb-4">
+              Real-time chat message web app (built while following a course tutorial).
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "TypeScript", "Supabase"].map((item, key) => (
+                <span
+                  key={key}
+                  className="bg-[#915c4f] text-white py-1 px-3 rounded-full text-sm hover:bg-[#915c4f]/50 hover:shadow-[0_2px_8px_rgba(145,92,79,0.2)] transition"
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+            <div className="flex justify-between items-center">
+              <a
                 href="https://chat-messaging-app-kappa.vercel.app"
                 target="_blank"
                 rel="noopener noreferrer"
@@ -126,6 +153,34 @@ const Projects = () => {
             </div>
           </div>
 
+          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
+            <h3 className="text-xl font-bold mb-2">Project Manager App</h3>
+            <p className="text-gray-400 mb-4">
+              React app to help users organize their current projects (built while following a course tutorial).
+            </p>
+            <div className="flex flex-wrap gap-2 mb-4">
+              {["React", "Tailwind CSS"].map((item, key) => (
+                <span
+                  key={key}
+                  className="bg-[#915c4f] text-white py-1 px-3 rounded-full text-sm hover:bg-[#915c4f]/50 hover:shadow-[0_2px_8px_rgba(145,92,79,0.2)] transition"
+                >
+                  {item}
+                </span>
+              ))}
+            </div>
+            <div className="flex justify-between items-center">
+              <a
+                href="https://saraccmululo.github.io/Project-Management-Webpage/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-[#b3472c] hover:text-[#915c4f] transition-colors my-4"
+              >
+                View Project →{" "}
+              </a>
+            </div>
+          </div>
+
+          {/*
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
             <h3 className="text-xl font-bold mb-2">Game Collection Web App</h3>
             <p className="text-gray-400 mb-4">
@@ -153,33 +208,7 @@ const Projects = () => {
               </a>
             </div>
           </div>
-          
-          {/*  
-          <h3 className="text-xl font-bold mb-2">Chat Messaging Web App</h3>
-            <p className="text-gray-400 mb-4">
-              Real-time chat message web app (built while following a course tutorial).
-            </p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "TypeScript", "Supabase"].map((item, key) => (
-                <span
-                  key={key}
-                  className="bg-[#915c4f] text-white py-1 px-3 rounded-full text-sm hover:bg-[#915c4f]/50 hover:shadow-[0_2px_8px_rgba(145,92,79,0.2)] transition"
-                >
-                  {item}
-                </span>
-              ))}
-            </div>
-            <div className="flex justify-between items-center">
-              <a
-                href="https://chat-messaging-app-kappa.vercel.app"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#b3472c] hover:text-[#915c4f] transition-colors my-4"
-              >
-                View Project →
-              </a>
-            </div>
-          </div>
+  
 
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
             <h3 className="text-xl font-bold mb-2">Quiz App</h3>
@@ -209,32 +238,7 @@ const Projects = () => {
           </div>
         */}
 
-          <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
-            <h3 className="text-xl font-bold mb-2">Project Manager App</h3>
-            <p className="text-gray-400 mb-4">
-              Project manager to help users track, edit, and delete tasks (built while following a course tutorial).
-            </p>
-            <div className="flex flex-wrap gap-2 mb-4">
-              {["React", "Tailwind CSS"].map((item, key) => (
-                <span
-                  key={key}
-                  className="bg-[#915c4f] text-white py-1 px-3 rounded-full text-sm hover:bg-[#915c4f]/50 hover:shadow-[0_2px_8px_rgba(145,92,79,0.2)] transition"
-                >
-                  {item}
-                </span>
-              ))}
-            </div>
-            <div className="flex justify-between items-center">
-              <a
-                href="https://saraccmululo.github.io/Project-Management-Webpage/"
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-[#b3472c] hover:text-[#915c4f] transition-colors my-4"
-              >
-                View Project →{" "}
-              </a>
-            </div>
-          </div>
+          
           {/* 
           <div className="p-6 rounded-xl border border-white/10 hover:-translate-y-1 hover:border-[#915c4f]/50 hover: shadow-[0_2px_8px_rgba(145,92,79,0.1)] transition-all ">
            
